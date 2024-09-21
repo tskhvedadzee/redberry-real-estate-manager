b4b10492abf82edbac2643fafca79f871066b6d7
tskhvedadze <tskhvedadzee@gmail.com>
2024-09-21 10:32:47 +0400

    feature(addListing page)

--- src/Pages/AddListing/AddListing.tsx
+++ src/Pages/AddListing/AddListing.tsx
@@ -1,7 +1,46 @@
-const AddListing = () => {
-  return (
-    <div>AddListing</div>
-  )
-}
+import { useNavigate } from "react-router-dom";
 
-export default AddListing
+import { Container } from "../../components/Container/Container.styled";
+import ListingRadioButtons from "./ListingRadioButtons/ListingRadioButtons";
+import ListingLocation from "./ListingLocation/ListingLocation";
+import ListingDetails from "./ListingDetails/ListingDetails";
+import ActionButton from "../../components/ActionButton/ActionButton";
+import ChooseAgent from "./ChooseAgent/ChooseAgent";
+
+import "./AddListing.scss";
+
+const AddListing = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+  return (
+    <Container>
+      <div className="add-listing">
+        <h1>ლისტინგის დამატება</h1>
+
+        <div className="add-listing__fields">
+
+          <ListingRadioButtons />
+          <ListingLocation />
+          <ListingDetails />
+          <ChooseAgent />
+
+          <div className="add-listing__buttons">
+            <ActionButton text="გაუქმება" onClick={handleBackClick} />
+            <ActionButton
+              primary
+              text="დაამატე ლისტინგი"
+              onClick={() => {
+                console.log("დაამატე ლისტინგი");
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default AddListing;
