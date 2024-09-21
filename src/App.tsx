fde37a1d259dad4d2a0dedf7c971f0cfeed3b2eb
tskhvedadze <tskhvedadzee@gmail.com>
2024-09-21 21:00:41 +0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { PublicLayout } from "./layout/PublicLayout";
 
-// import Home from "./pages/Home/Home";
-// import AddListing from "./pages/AddListing/AddListing";
-// import ListingPage from "./pages/ListingPage/ListingPage";
+const Home = lazy(() => import("./routes/Home/Home"));
+const AddListing = lazy(() => import("./routes/AddListing/AddListing"));
+const ListingPage = lazy(() => import("./routes/ListingPage/ListingPage"));
 
 import "./App.css";
 
@@ -13,9 +13,9 @@ function App() {
     <Suspense>
       <Routes>
         <Route element={<PublicLayout />}>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/add-listing" element={<AddListing />} />
-          <Route path="/listing-page" element={<ListingPage />} /> */}
+          <Route path="/listing-page" element={<ListingPage />} />
         </Route>
       </Routes>
     </Suspense>
