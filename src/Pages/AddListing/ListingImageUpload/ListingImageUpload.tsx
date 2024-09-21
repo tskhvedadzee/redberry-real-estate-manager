13c1a511474f99b9f086edb25dd4c6b3ab303629
tskhvedadze <tskhvedadzee@gmail.com>
2024-09-21 10:31:11 +0400

    feature(imageUpload component)

---
+++ src/Pages/AddListing/ListingImageUpload/ListingImageUpload.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import ImageUploading, { ImageListType } from "react-images-uploading";
+import { CiCirclePlus } from "react-icons/ci";
+
+import "./ListingImageUpload.scss";
+
+export function ListingImageUpload() {
+  const [images, setImages] = React.useState([]);
+
+  const onChange = (
+    imageList: ImageListType,
+    addUpdateIndex: number[] | undefined
+  ) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList as never[]);
+  };
+
+  return (
+    <div className="image-upload">
+      <ImageUploading
+        value={images}
+        onChange={onChange}
+        maxNumber={1}
+        maxFileSize={1000000}
+      >
+        {({ imageList, onImageUpload, isDragging, dragProps }) => (
+          <div className="image-upload__wrapper">
+            <span className="image-upload__title">ატვირთეთ ფოტო</span>
+            <div
+              className="image-upload__field"
+              style={isDragging ? { color: "red" } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              <div className="image-upload__container">
+                {imageList.length === 0 ? (
+                  <CiCirclePlus className="image-upload__upload-icon" />
+                ) : (
+                  <>
+                    {imageList.map((image, index) => (
+                      <div key={index} className="image-upload__image-item">
+                        <img
+                          src={image.dataURL}
+                          alt=""
+                          className="image-upload__uploaded-image"
+                        />
+                      </div>
+                    ))}
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
+}
