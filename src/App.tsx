054f2b1dfdbb71eb3b772fa6b71d01df1ff08364
tskhvedadze <tskhvedadzee@gmail.com>
2024-09-21 20:59:27 +0400

    fix(hotfix)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { PublicLayout } from "./layout/PublicLayout";
 
-import Home from "./pages/Home/Home";
-import AddListing from "./pages/AddListing/AddListing";
-import ListingPage from "./pages/ListingPage/ListingPage";
+// import Home from "./pages/Home/Home";
+// import AddListing from "./pages/AddListing/AddListing";
+// import ListingPage from "./pages/ListingPage/ListingPage";
 
 import "./App.css";
 
@@ -13,9 +13,9 @@ function App() {
     <Suspense>
       <Routes>
         <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="/add-listing" element={<AddListing />} />
-          <Route path="/listing-page" element={<ListingPage />} />
+          <Route path="/listing-page" element={<ListingPage />} /> */}
         </Route>
       </Routes>
     </Suspense>
